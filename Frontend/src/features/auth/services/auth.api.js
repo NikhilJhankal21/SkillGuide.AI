37ccd7bde750586ab23f94cfc98653c077aac663
NikhilJhankal21 <nikhiljhankal@gmail.com>
2026-08-28 01:00:52 +0530

    Update CORS for Vercel frontend

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
+    baseURL: "https://skillguide-ai.onrender.com",
     withCredentials: true
 })
 
